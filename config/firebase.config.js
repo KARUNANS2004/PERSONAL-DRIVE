fa68396b2a59e26afdd1e60991fc7dc1d0c81421
KARUNANS2004 <s.karuna0074@gmail.com>
2025-07-12 19:01:43 +0530

    update

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -2,7 +2,12 @@ const Firebase=require('firebase-admin')
 const serviceAccount=require('../drive-clone-e70b3-firebase-adminsdk-3fyxq-968b6f6603.json')
 
 const firebase=Firebase.initializeApp({
-    credential:Firebase.credential.cert(serviceAccount),
+    credential:Firebase.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        serviceAccount
+    }),
     storageBucket:'drive-clone-e70b3.firebasestorage.app',
 })
 
